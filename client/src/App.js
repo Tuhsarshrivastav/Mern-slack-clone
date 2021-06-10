824946cef693ebef93b6b92e26026f91be6311c6
Tusharshrivastav <ts3657@gmail.com>
2021-06-10 11:59:50 +0530

    add footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import NavbarContainer from "./components/Layout/Navbar/NavbarContainer";
 import { BrowserRouter as Router } from "react-router-dom";
+import Footer from "./components/Layout/Footer";
 const App = () => {
   return (
     <Router>
       <NavbarContainer />
+      <Footer/>
     </Router>
   );
 };
---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <div className="container-fluid my-5 bg-dark">
+      <div className="container text-center text-white">
+        <img
+          src="assets/img/slack_logo.png"
+          alt="logo"
+          width="40"
+          height="40"
+          className="rounded-circle me-2"
+        />
+        Slack-clone @2021
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
